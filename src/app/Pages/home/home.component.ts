e2ab1582aeaef5403b95e081201e343b4ea8071e
“name“omar <121135630+OmarAbd-Almaksoud@users.noreply.github.com>
2024-02-23 18:57:37 +0200

    Grid system for home page

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Components/navbar/navbar.component';
+import { FooterComponent } from "../../Components/footer/footer.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [NavbarComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [NavbarComponent, FooterComponent]
 })
 export class HomeComponent {}
