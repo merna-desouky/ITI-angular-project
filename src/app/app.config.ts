a2d8694d0e2cb760a697e9ba5019ca822f02d067
“name“omar <121135630+OmarAbd-Almaksoud@users.noreply.github.com>
2024-02-26 13:05:17 +0200

    booking page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from "@angular/platform-browser/animations"
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [provideRouter(routes), provideAnimations(), provideAnimationsAsync()]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './Pages/home/home.component';
 import { SignUpComponent } from './Pages/sign-up/sign-up.component';
 import { SignInComponent } from './Pages/sign-in/sign-in.component';
 import { ErrorComponent } from './Pages/error/error.component';
+import { BookYourTicketComponent } from './Pages/book-your-ticket/book-your-ticket.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'sign-in', component: SignInComponent },
+  { path: 'book-ticket', component: BookYourTicketComponent },
   { path: '**', component: ErrorComponent },
 ];
