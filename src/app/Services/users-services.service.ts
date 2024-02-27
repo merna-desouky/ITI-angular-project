fcff047c8f66de455c3d491725e6d8a250476c95
ahmedalam98 <ahmed.alameldeen,me@gmail.com>
2024-02-27 18:53:37 +0200

    refactor: :recycle: Fix body font

--- src/app/Services/users-services.service.ts
+++ src/app/Services/users-services.service.ts
@@ -3,28 +3,26 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersServicesService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   Register(userData: {}): Observable<any> {
-    return this.http.post("http://localhost:2024/auth/register", userData, {
+    return this.http.post('http://localhost:2024/auth/register', userData, {
       headers: new HttpHeaders({
-        accept: 'application/json'
-      })
-    })
+        accept: 'application/json',
+      }),
+    });
   }
   Login(userData: {}): Observable<any> {
-    return this.http.post("http://localhost:2024/auth/login", userData, {
+    return this.http.post('http://localhost:2024/auth/login', userData, {
       headers: new HttpHeaders({
-        accept: 'application/json'
-      })
-    })
+        accept: 'application/json',
+      }),
+    });
   }
-  Logout():void{
-    localStorage.clear()
+  Logout(): void {
+    localStorage.clear();
   }
-
 }
