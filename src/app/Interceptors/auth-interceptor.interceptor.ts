5ec1f224a23c0dd9ee59e3ac937e161f613fffa4
ahmedalam98 <ahmed.alameldeen,me@gmail.com>
2024-02-28 23:51:53 +0200

    refactor: :truck: Move & Rename resources

--- src/app/Interceptors/auth-interceptor.interceptor.ts
+++ src/app/Interceptors/auth-interceptor.interceptor.ts
@@ -4,30 +4,28 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpHeaders
+  HttpHeaders,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthServiceService } from '../Services/auth-service.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authservice: AuthServiceService) {}
 
-  constructor(private authservice: AuthServiceService) { }
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    var reqWithToken = request
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    var reqWithToken = request;
     let token = this.authservice.isLoggedIn();
 
     if (token) {
-
       let header = new HttpHeaders({
         Authorization: `Bearer ${token}`,
-
-
-      })
-      reqWithToken = request.clone({ headers: header })
+      });
+      reqWithToken = request.clone({ headers: header });
       return next.handle(reqWithToken);
-
     }
     return next.handle(reqWithToken);
   }
