95c23386c9a16f8a4a82a65607176e77daf11c2b
Merna21 <mernadesoky@gmail.com>
2024-02-28 14:10:00 +0200

    feat: :fire: Add search function

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,11 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+import { MoviesService } from '../../Services/movies.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, HttpClientModule, RouterLink],
+  providers: [MoviesService],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  movies: any;
+  item: any;
+  resultMovies: any[] = [];
+  showResults: boolean = false;
+  myInput: any
+  @ViewChild('targetDiv', { static: false }) targetDivRef!: ElementRef;
+
+  constructor(private moviesService: MoviesService) { }
+
+
+  ngOnInit(): void {
+    this.moviesService.getAllMovies().subscribe({
+      next: (data) => {
+        this.movies = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  result(evt: any) {
+    this.resultMovies = [];
+    this.showResults = true;
+    if (this.item) {
+      for (let i = 0; i < this.movies?.length; i++) {
+        if (this.movies[i].Title.toLowerCase().startsWith(this.item.toLowerCase())) {
+          this.resultMovies.push(this.movies[i]);
+          console.log(this.resultMovies, "sssss");
+        }
+      }
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleDocumentClick(event: Event) {
+    console.log(this.targetDivRef.nativeElement)
+    if (
+      this.targetDivRef &&
+      this.targetDivRef.nativeElement &&
+      !this.targetDivRef.nativeElement.contains(event.target as Node)
+    ) {
+      this.showResults = false;
+      // this.resultMovies = []
+      this.item = ""
+    }
+  }
+}
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { NavbarComponent } from '../../Components/navbar/navbar.component';
 import { FooterComponent } from '../../Components/footer/footer.component';
 import { MoviesService } from '../../Services/movies.service';
@@ -7,7 +7,7 @@ import { FilterComponent } from '../../Components/filter/filter.component';
 import { MoviesCardsComponent } from '../../Components/movies-cards/movies-cards.component';
 import { CarouselComponent } from '../../Components/carousel/carousel.component';
 import { HeroComponent } from '../../Components/hero/hero.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -21,21 +21,42 @@ import { HeroComponent } from '../../Components/hero/hero.component';
     CarouselComponent,
     HttpClientModule,
     FilterComponent,
-    HeroComponent,
+    HeroComponent, CommonModule
   ],
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class HomeComponent implements OnInit {
   movies: any;
-  constructor(private moviesService: MoviesService) {}
+  selectedCatg = "All"
+  filterdMovies: any
+  constructor(private moviesService: MoviesService) { }
   ngOnInit(): void {
     this.moviesService.getAllMovies().subscribe({
       next: (data) => {
         this.movies = data;
-        console.log(this.movies);
+        this.filterdMovies = data;
       },
       error: (err) => {
         console.log(err);
       },
     });
+
+  }
+  getSelectedCatg(evt: any) {
+    this.selectedCatg = evt;
+    console.log(this.selectedCatg, 'dataa');
+    this.filterdMovies = [];
+
+    for (let i = 0; i < this.movies?.length; i++) {
+      if (this.selectedCatg === "All" || this.selectedCatg === null) {
+        this.filterdMovies.push(this.movies[i])
+
+      }
+      else if (this.selectedCatg === this.movies[i].Genre) {
+        this.filterdMovies.push(this.movies[i])
+      }
+
+    }
   }
 }
