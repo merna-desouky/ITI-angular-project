75122eb2be24d9100558cb329548754e76b2da35
ahmedalam98 <ahmed.alameldeen,me@gmail.com>
2024-03-03 19:24:18 +0200

    style: :art: Refactor navbar + pagination

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,12 @@ import { SignInComponent } from './Pages/sign-in/sign-in.component';
 import { ErrorComponent } from './Pages/error/error.component';
 import { BookingComponent } from './Pages/booking/booking.component';
 import { SingleMovieComponent } from './Components/single-movie/single-movie.component';
-import { CheckoutComponent } from './Components/checkout/checkout.component';
+import { CheckoutComponent } from './Pages/checkout/checkout.component';
 import { ProfileComponent } from './Pages/profile/profile.component';
 
 export const routes: Routes = [
   // { path: '', component: HomeComponent , canActivate:[canActivate]},
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
   { path: 'movie/:id', component: SingleMovieComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'sign-in', component: SignInComponent },
