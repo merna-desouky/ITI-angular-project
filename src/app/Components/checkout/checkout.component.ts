cccc36839487ed9f1b7de1b44dc2099a4f8a1c46
ahmedalam98 <ahmed.alameldeen,me@gmail.com>
2024-03-03 17:11:33 +0200

    feat: :sparkles: Integrate cart endpoints

--- src/app/Components/checkout/checkout.component.ts
+++ src/app/Components/checkout/checkout.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../Services/movies.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../Services/cart.service';
 import {
   FormGroup,
   FormBuilder,
@@ -14,44 +15,63 @@ import {
   selector: 'app-checkout',
   standalone: true,
   imports: [HttpClientModule, RouterModule, CommonModule, FormsModule],
-  providers: [MoviesService],
+  providers: [MoviesService, CartService],
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
   // Data will be fetched from the query string + service
-
-  // Dummy data
-  movies: any[] = [];
-  checkoutMoviesIds: number[] = [2, 3];
-
   paymentOption: string = '';
+  movies: any[] = [];
+  userCart: any;
 
-  testID = 1;
-
-  constructor(private moviesService: MoviesService) {}
+  constructor(
+    private moviesService: MoviesService,
+    private cartService: CartService,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
-    this.loadDummyMovies();
-  }
-
-  private loadDummyMovies(): void {
-    this.checkoutMoviesIds.forEach((id) => {
-      this.moviesService.getMovieById(id).subscribe({
-        next: (data: any) => {
-          const checkoutMovie = {
-            id: data.id,
-            Poster: data.Poster,
-            Title: data.Title,
-            totalSeats: 5,
-            totalPrice: 500,
-          };
-          this.movies.push(checkoutMovie);
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
+    this.cartService.getUserCart().subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.userCart = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
+
+  checkoutUserCart(cart: any) {
+    this.cartService.checkoutUserCart(cart).subscribe({
+      next: (data: any) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  // Disable checkout if the user cart is empty
 }
+// private loadDummyMovies(): void {
+//   this.checkoutMoviesIds.forEach((id) => {
+//     this.moviesService.getMovieById(id).subscribe({
+//       next: (data: any) => {
+//         const checkoutMovie = {
+//           id: data.id,
+//           Poster: data.Poster,
+//           Title: data.Title,
+//           totalSeats: 5,
+//           totalPrice: 500,
+//         };
+//         this.movies.push(checkoutMovie);
+//       },
+//       error: (err) => {
+//         console.log(err);
+//       },
+//     });
+//   });
+// }
