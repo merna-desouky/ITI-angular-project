a919e5b21be4d641c7b9969bff62307a47b2489c
Aml <amlmahdawy21@gmail.com>
2024-03-08 03:45:16 +0200

    authAdmin

--- src/app/Pages/sign-in/sign-in.component.ts
+++ src/app/Pages/sign-in/sign-in.component.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/forms';
 import { UsersServicesService } from '../../Services/users-services.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AuthServiceService } from '../../Services/auth-service.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -23,7 +24,8 @@ export class SignInComponent implements OnInit {
   constructor(
     private usersService: UsersServicesService,
     private http: HttpClient,
-    private route: Router
+    private route: Router,
+    private AuthService:AuthServiceService
   ) {}
   /////////////////////////////////////////////////////////////////////////
 
@@ -109,9 +111,17 @@ export class SignInComponent implements OnInit {
     if (this.signInForm.valid) {
       this.usersService.Login(this.signInForm.value).subscribe(
         (data) => {
+         let decodedToken= this.AuthService.DecodedToken(data.token)
+         if(decodedToken.isAdmin){
+          localStorage.setItem('token', data.token);
+          this.route.navigate(['/dashboard']);
+
+         }else{
           localStorage.setItem('token', data.token);
           sessionStorage.removeItem('loggedInUser');
           this.route.navigate(['/']);
+         }
+          
         },
         (err) => {
           console.log(err.error.message);
